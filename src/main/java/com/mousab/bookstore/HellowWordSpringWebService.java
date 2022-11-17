2b3dddd0562d19da6e64adf9c99e6e0cbed2e7b0
najeebmosab <najeebmosab@gmail.com>
2022-11-17 19:24:02 +0200

    this is my first task0

--- src/main/java/com/mousab/bookstore/HellowWordSpringWebService.java
+++ src/main/java/com/mousab/bookstore/HellowWordSpringWebService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HellowWordSpringWebService {
     @GetMapping
     public String getHelloWord(){
-        return  "Hello Word";
+        return  "Hello Word!!";
     }
 
 }
